e88e04f177e5e684e093cf5c6991b6db6694bcb7
System Administrator <root@MacBook-Pro-de-Maicon.local>
2021-07-05 10:06:05 -0300

    Add GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
+import Router from 'next/router';
+import React, {useEffect } from 'react';
+import { GTMPageView } from '../utils/gtm';
 
 function MyApp({ Component, pageProps }) {
+    // Initiate GTM
+    useEffect(() => {
+      const handleRouteChange = (url: string) => GTMPageView(url);
+      Router.events.on('routeChangeComplete', handleRouteChange);
+      return () => {
+          Router.events.off('routeChangeComplete', handleRouteChange);
+      };
+  }, []);
   return <Component {...pageProps} />
 }
 
